650d3030a656eb76e451a1544df21e3f9021a9fd
Julian van Riet <vanrietjulian@gmail.com>
2014-11-05 08:45:00 -0800

    Creating Blog Post Form

---
+++ post.php
@@ -0,0 +1,10 @@
+<?php 
+
+	require_once(__DIR__ . "/view/header.php");
+	//similar to index requirement, links to header.php
+
+	require_once(__DIR__ . "/view/footer.php");
+	//similar to index, make footer usable
+
+	require_once(__DIR__ . "/view/form.php");
+	//require form for the use of creating blog post
---
+++ view/form.php
@@ -0,0 +1,17 @@
+<h1>Create Blog Post</h1>
+<!-- at top text says this^-->
+
+<form>
+	<div>
+		<label for="title">Title: </label>
+		<!-- gives first text box title -->
+		<input type="text" name="title" />
+		<!-- creates small text box -->
+	</div>
+	<div>
+		<label for="post">Post: </label>
+		<!-- names text area post -->
+		<textarea name="post"></textarea>
+		<!-- text area bigger text box than -->
+	</div>
+</form>
