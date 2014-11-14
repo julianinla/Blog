20e6123ce731b8793276f756d220dd355883e9fc
Julian van Riet <vanrietjulian@gmail.com>
2014-11-14 08:51:52 -0800

    Establishing a Database Connection

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,5 +1,12 @@
 <?php 
 
+	require_once(__DIR__ . "/../model/database.php");
+	//connecting file to the database
+	//gives access to sqli vars
+
+	$connection = new mysqli($host, $username, $password, $database);
+	//created connection with now accessible vars
+
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	//filters malicious input for our titles
 	//santizes and stops hacking our blog for most part
