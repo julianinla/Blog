25ed094b4e5336526daea88c449ba6ac79ef0dd5
Julian van Riet <vanrietjulian@gmail.com>
2014-11-17 10:44:01 -0800

    Creating Functions with our Database Class

--- model/Database.php
+++ model/Database.php
@@ -16,8 +16,29 @@
 		//initiliazing variables similar to above
 		//all are private 
 
-		public function 
-		//function to be accessed for new obj creation
+		public function __construct($host, $username, $password, $database) {
+			$this->host = $host; 
+			$this->username = $username;
+			$this->password = $password;
+			$this->database = $database;
+			//accesses the host variable
+		}
+		//construct function to be accessed for new obj creation
+
+		public function openConnection() {
+
+		}
+		//to replace opening connection
+
+		public function closeConnection() {
+			
+		}
+		//to replace closing connection
+
+		public function query($string) /* passing through string for query */ {
+			
+		}
+		//to replace query code
 	}
 	//the class is being used for its convenience in refactoring our code
 	//if we use the class then each time less code is being written 
