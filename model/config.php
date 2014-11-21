35ba4bc6f1efe8f29188bb7db82bccd9f9cf449e
Julian van Riet <vanrietjulian@gmail.com>
2014-11-21 09:02:38 -0800

    Using Sessions to Store Data

--- model/config.php
+++ model/config.php
@@ -1,6 +1,9 @@
 <?php
 
 	require_once(__DIR__ . "/Database.php");
+	//require once for database
+	session_start();
+	//use for session var $connection
 
 	$path = "/Blog/";
 	//used for fixing path for future changes
@@ -12,7 +15,12 @@
 	$database = "blog_db"; //database variable
 	//initalizing required variables for database
 
-	//need to create new database object based on class
-	$connection = new Database($host, $username, $password, $database);
-	//passing variables
-	//going to help query database
+	if(isset($_SESSION["connection"])) { /* want to determine if var has value */
+		//need to create new database object based on class
+		$connection = new Database($host, $username, $password, $database);
+		//passing variables
+		//going to help query database
+
+		$_SESSION["connection"] = $connection;
+		//assigning connection var to session var
+	}
