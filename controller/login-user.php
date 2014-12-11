9ea2d6c0db4b38cda3d5a7feb7528a3a264bfa30
Julian van Riet <vanrietjulian@gmail.com>
2014-12-10 20:08:03 -0800

    Selecting User Information From the Database

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,2 +1,9 @@
 <?php
-	echo "Successfully logged in user";
+	require_once(__DIR__ . /../model/config.php);
+	//need to config file
+
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+	//need to filter input for login
+
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
