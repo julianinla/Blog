9d2cdaeb1014ba30376539411656492f3a92ba75
Julian van Riet <vanrietjulian@gmail.com>
2014-12-12 00:07:08 -0800

    Protecting Controllers and Views

--- view/form.php
+++ view/form.php
@@ -2,6 +2,13 @@
 
 	require_once(__DIR__ . "/../model/config.php");
 
+	require_once(__DIR__ . "/../controller/login-verify.php");
+
+	if(!authenticateUser()) {
+		header("Location: " . $path . "index.php");
+		die();
+	}
+
 ?>
 
 <h1 align="center">Create Blog Post</h1>
--- view/navigation.php
+++ view/navigation.php
@@ -1,6 +1,13 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php");
 	//accesses $path in config.php
+
+	require_once(__DIR__ . "/../controller/login-verify.php");
+
+	if(!authenticateUser()) {
+		header("Location: " . $path . "index.php");
+		die();
+	}
 ?>
 
 <nav>
