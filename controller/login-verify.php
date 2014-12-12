9d6e8fe4667b54c0ea1bf3620143f8e971aeee98
Julian van Riet <vanrietjulian@gmail.com>
2014-12-11 23:55:06 -0800

    Checking for User Login

---
+++ controller/login-verify.php
@@ -0,0 +1,17 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+
+	function authenticateUser() {
+		if(!isset($_SESSION["authenticated"])) {
+			return false;
+		}
+		//checks whther it is set, user logged in
+		else {
+			if($_SESSION["authenticated"] != true) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+	}
--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 	<?php 
 		//not html file
 
+		require_once(__DIR__ . "/controller/login-verify.php");
+
 		require_once(__DIR__ . "/view/header.php");
 		//allows us to access the header code
-
-		require_once(__DIR__ . "/view/navigation.php");
-		//gives access to nav.php for use in blog
+		
+		if(authenticateUser()) {
+			require_once(__DIR__ . "/view/navigation.php");
+			//gives access to nav.php for use in blog
+		}
 
 		require_once(__DIR__ . "/view/header.php");
 		//allows us to access the header code
